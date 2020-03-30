2b19237db27228584143656a0160e4405e0663b5
123liqiuping <61663152+123liqiuping@users.noreply.github.com>
2020-03-30 08:52:10 +0800

    3-10-1

---
+++ m1/MFC2View.cpp
@@ -0,0 +1,119 @@
+﻿
+// MFC2View.cpp: CMFC2View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC2.h"
+#endif
+
+#include "MFC2Doc.h"
+#include "MFC2View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC2View
+
+IMPLEMENT_DYNCREATE(CMFC2View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC2View, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+	ON_WM_LBUTTONUP()
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// CMFC2View 构造/析构
+
+CMFC2View::CMFC2View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC2View::~CMFC2View()
+{
+}
+
+BOOL CMFC2View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC2View 绘图
+
+void CMFC2View::OnDraw(CDC* /*pDC*/)
+{
+	CMFC2Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+void CMFC2View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMFC2View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMFC2View 诊断
+
+#ifdef _DEBUG
+void CMFC2View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC2View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC2Doc* CMFC2View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC2Doc)));
+	return (CMFC2Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC2View 消息处理程序
+
+
+void CMFC2View::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CString r = _T("左键正在抬起");//第三题
+	CClientDC dc(this);
+	dc.TextOutW(300, 300, r);
+	CView::OnLButtonUp(nFlags, point);
+}
+
+
+void CMFC2View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CString l = _T("左键正在按下");//第三题
+	CClientDC dc(this);
+	dc.TextOutW(100, 200, l);
+	CView::OnLButtonDown(nFlags, point);
+}
