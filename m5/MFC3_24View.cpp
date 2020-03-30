5c047141b266a378608a9110fb0504a66a8fe9e4
123liqiuping <61663152+123liqiuping@users.noreply.github.com>
2020-03-30 09:24:12 +0800

    3-24-1

---
+++ m5/MFC3_24View.cpp
@@ -0,0 +1,107 @@
+﻿
+// MFC3_24View.cpp: CMFC324View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC3_24.h"
+#endif
+
+#include "MFC3_24Doc.h"
+#include "MFC3_24View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC324View
+
+IMPLEMENT_DYNCREATE(CMFC324View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC324View, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+	ON_COMMAND(ID_VIEW_MINGZI, &CMFC324View::OnViewMingzi)
+END_MESSAGE_MAP()
+
+// CMFC324View 构造/析构
+
+CMFC324View::CMFC324View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC324View::~CMFC324View()
+{
+}
+
+BOOL CMFC324View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC324View 绘图
+
+void CMFC324View::OnDraw(CDC* /*pDC*/)
+{
+	CMFC324Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+void CMFC324View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMFC324View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMFC324View 诊断
+
+#ifdef _DEBUG
+void CMFC324View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC324View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC324Doc* CMFC324View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC324Doc)));
+	return (CMFC324Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC324View 消息处理程序
+
+
+void CMFC324View::OnViewMingzi()
+{
+	// TODO: 在此添加命令处理程序代码
+	CString s = _T("李秋萍");
+	CClientDC dc(this);
+	dc.TextOutW(200,200,s);
+}
---
+++ m5/MFC3_24View.h
@@ -0,0 +1,51 @@
+﻿
+// MFC3_24View.h: CMFC324View 类的接口
+//
+
+#pragma once
+
+
+class CMFC324View : public CView
+{
+protected: // 仅从序列化创建
+	CMFC324View() noexcept;
+	DECLARE_DYNCREATE(CMFC324View)
+
+// 特性
+public:
+	CMFC324Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMFC324View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnViewMingzi();
+};
+
+#ifndef _DEBUG  // MFC3_24View.cpp 中的调试版本
+inline CMFC324Doc* CMFC324View::GetDocument() const
+   { return reinterpret_cast<CMFC324Doc*>(m_pDocument); }
+#endif
+
