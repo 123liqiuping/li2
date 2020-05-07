cde3b03977e46b25ef77252067c84cc0de15ae86
123liqiuping <61663152+123liqiuping@users.noreply.github.com>
2020-05-07 08:06:17 +0800

    5_4_3

---
+++ m38/W32D/W32D.cpp
@@ -0,0 +1,42 @@
+﻿// W32D.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "W32D.h"
+
+
+// 这是导出变量的一个示例
+W32D_API int nW32D=0;
+W32D_API int factorial(int n)
+{
+	int i, j = 1;
+	if (n < 10)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			j *= i;
+		}
+		return j;
+	}
+}
+float FAC::convert(float deg)
+{
+	float pi = 3.14159265f;
+	float j;
+	j = deg / 180 * pi;
+	return j;
+}
+// 这是导出函数的一个示例。
+W32D_API int fnW32D(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+CW32D::CW32D()
+{
+    return;
+}
+
+
