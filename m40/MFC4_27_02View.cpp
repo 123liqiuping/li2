59ea7e428aeb62f8e2c0b8a911641e6c0c649f61
123liqiuping <61663152+123liqiuping@users.noreply.github.com>
2020-05-09 17:16:18 +0800

    4_27_2

---
+++ m40/MFC4_27_02View.cpp
@@ -0,0 +1,102 @@
+﻿
+// MFC4_27_02View.cpp: CMFC42702View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC4_27_02.h"
+#endif
+
+#include "MFC4_27_02Doc.h"
+#include "MFC4_27_02View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC42702View
+
+IMPLEMENT_DYNCREATE(CMFC42702View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC42702View, CView)
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// CMFC42702View 构造/析构
+
+CMFC42702View::CMFC42702View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC42702View::~CMFC42702View()
+{
+}
+
+BOOL CMFC42702View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC42702View 绘图
+
+void CMFC42702View::OnDraw(CDC* pDC)
+{
+	CMFC42702Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+	pDC->Ellipse(pDoc->cr);
+
+
+}
+
+
+// CMFC42702View 诊断
+
+#ifdef _DEBUG
+void CMFC42702View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC42702View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC42702Doc* CMFC42702View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC42702Doc)));
+	return (CMFC42702Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC42702View 消息处理程序
+
+
+void CMFC42702View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CView::OnLButtonDown(nFlags, point);
+	CMFC42702Doc* pDoc = GetDocument();
+	pDoc->cr.left += 5;
+	pDoc->cr.top += 5;
+	pDoc->cr.right -= 5;
+	pDoc->cr.bottom -= 5;
+	InvalidateRect(NULL, FALSE);
+	CView::OnLButtonDown(nFlags, point);
+
+}
---
+++ m40/MFC4_27_02View.h
@@ -0,0 +1,48 @@
+﻿
+// MFC4_27_02View.h: CMFC42702View 类的接口
+//
+
+#pragma once
+
+
+class CMFC42702View : public CView
+{
+protected: // 仅从序列化创建
+	CMFC42702View() noexcept;
+	DECLARE_DYNCREATE(CMFC42702View)
+
+// 特性
+public:
+	CMFC42702Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMFC42702View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // MFC4_27_02View.cpp 中的调试版本
+inline CMFC42702Doc* CMFC42702View::GetDocument() const
+   { return reinterpret_cast<CMFC42702Doc*>(m_pDocument); }
+#endif
+
