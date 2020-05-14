61d43fb28a8301c61ec24eb7a7141a820daa664b
123liqiuping <61663152+123liqiuping@users.noreply.github.com>
2020-05-14 09:54:00 +0800

    2_24

---
+++ m41/MFC2_24.h
@@ -0,0 +1,33 @@
+﻿
+// MFC2_24.h: MFC2_24 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMFC224App:
+// 有关此类的实现，请参阅 MFC2_24.cpp
+//
+
+class CMFC224App : public CWinApp
+{
+public:
+	CMFC224App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC224App theApp;
