4bc7b0e2670399568017e5d93a57d05bef44efa6
123liqiuping <61663152+123liqiuping@users.noreply.github.com>
2020-05-14 10:03:49 +0800

    3_02_4

---
+++ m45/MFC3_02_4View.cpp
@@ -0,0 +1,93 @@
+﻿
+// MFC3_02_4View.cpp: CMFC3024View 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFC3_02_4.h"
+#endif
+
+#include "MFC3_02_4Doc.h"
+#include "MFC3_02_4View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC3024View
+
+IMPLEMENT_DYNCREATE(CMFC3024View, CView)
+
+BEGIN_MESSAGE_MAP(CMFC3024View, CView)
+END_MESSAGE_MAP()
+
+// CMFC3024View 构造/析构
+
+CMFC3024View::CMFC3024View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFC3024View::~CMFC3024View()
+{
+}
+
+BOOL CMFC3024View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFC3024View 绘图
+
+void CMFC3024View::OnDraw(CDC* pDC)
+{
+	CMFC3024Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+	
+	
+	//int r, g, b;
+	CBrush brush(RGB(250, 0, 0));
+	CBrush* old;
+	old = pDC->SelectObject(&brush);
+	CRect cr;
+	this->GetClientRect(cr);
+	pDC->Ellipse(cr);
+	pDC->SelectObject(old);
+	this->ReleaseDC(pDC);
+}
+
+
+// CMFC3024View 诊断
+
+#ifdef _DEBUG
+void CMFC3024View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFC3024View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFC3024Doc* CMFC3024View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFC3024Doc)));
+	return (CMFC3024Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFC3024View 消息处理程序
---
+++ m45/MFC3_02_4View.h
@@ -0,0 +1,46 @@
+﻿
+// MFC3_02_4View.h: CMFC3024View 类的接口
+//
+
+#pragma once
+
+
+class CMFC3024View : public CView
+{
+protected: // 仅从序列化创建
+	CMFC3024View() noexcept;
+	DECLARE_DYNCREATE(CMFC3024View)
+
+// 特性
+public:
+	CMFC3024Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMFC3024View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MFC3_02_4View.cpp 中的调试版本
+inline CMFC3024Doc* CMFC3024View::GetDocument() const
+   { return reinterpret_cast<CMFC3024Doc*>(m_pDocument); }
+#endif
+
