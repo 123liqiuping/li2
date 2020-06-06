41422e0f6963e0b04b476b0c7a429d2ca6d54b65
123liqiuping <61663152+123liqiuping@users.noreply.github.com>
2020-06-06 19:06:56 +0800

    第九周实验课 第1题

---
+++ 第九周实验1/MFC9.h
@@ -0,0 +1,32 @@
+
+// MFC9.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFC9App: 
+// 有关此类的实现，请参阅 MFC9.cpp
+//
+
+class CMFC9App : public CWinApp
+{
+public:
+	CMFC9App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC9App theApp;
