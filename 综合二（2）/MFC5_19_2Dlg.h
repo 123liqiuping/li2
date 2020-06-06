6cd7b21ddc61ca8615f3a93982a3e841f01bf8da
123liqiuping <61663152+123liqiuping@users.noreply.github.com>
2020-06-06 19:14:37 +0800

    综合实验之二 第2题

---
+++ 综合二（2）/MFC5_19_2Dlg.h
@@ -0,0 +1,36 @@
+﻿
+// MFC5_19_2Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMFC5192Dlg 对话框
+class CMFC5192Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFC5192Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC5_19_2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CButton pic;
+};
